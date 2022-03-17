4339d7e7ee57b5feb40579e72bb4f88e71f6da31
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-17 23:39:56 +0530

    added function for uppercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function TextForm() {
+
+export default function TextForm(props) {
+    const upClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const onChange = (event) => {
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('Enter your text here');
+    // console.log(useState('Enter your text here'));
     return (
-        <div class="mb-3">
-            <label for="myTextBox" class="form-label">Text Area</label>
-            <textarea class="form-control" id="myTextBox" rows="3"></textarea>
+        <div>
+            <div className="mb-3">
+                <label htmlFor="myTextBox" className="form-label">Text Area</label>
+                <textarea className="form-control" id="myTextBox" value={text} onChange={onChange} rows="6"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={upClick}>Convert to uppercase</button>
         </div>
     )
 }
