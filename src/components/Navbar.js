74266b6240ea9ab4f1d6d05edba6f02ce747d931
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-17 22:31:46 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
@@ -11,10 +11,10 @@ export default function Navbar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">{props.title}</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">Link</a>
+                <a className="nav-link" href="/">{props.about}</a>
               </li>
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -23,7 +23,7 @@ export default function Navbar() {
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li><a className="dropdown-item" href="/">Action</a></li>
                   <li><a className="dropdown-item" href="/">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
+                  {/* <li><hr className="dropdown-divider"/></li> */}
                   <li><a className="dropdown-item" href="/">Something else here</a></li>
                 </ul>
               </li>
