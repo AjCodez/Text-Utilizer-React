b77ad8398627aa4fcb6f2f3f9e0254842b400204
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-18 19:49:35 +0530

    added feature for dark theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = () => {
+      if(mode==='light'){
+        setMode('dark');
+        document.body.style.backgroundColor= 'rgb(38 38 70)';
+      }
+      else{
+        setMode('light');
+        document.body.style.backgroundColor= 'white';
+      }
+  }
   return (
     <>
-      <Navbar title="Hello Aj" about="About Aj"/>
+      <Navbar title="Hello Aj" about="About Aj" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-      <TextForm/> 
+      <TextForm mode={mode}/> 
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,18 +18,20 @@ export default function TextForm(props) {
     return (
         <>
             <div>
-                <div className="mb-3">
-                    <label htmlFor="myTextBox" className="form-label">Text Area</label>
-                    <textarea className="form-control" id="myTextBox" value={text} onChange={onChange} rows="6"></textarea>
+                <div className="mb-3" style= {{color:props.mode==='light'?'black':'white'}}>
+                    <label htmlFor="myTextBox" className="form-label"><h4 style= {{color:props.mode==='light'?'black':'white'}}>Text Area</h4></label>
+                    <textarea className="form-control" id="myTextBox" value={text} onChange={onChange} placeholder="Enter text here" style= {{color:props.mode==='light'?'black':'white',  background:props.mode==='light'?'white':'#b2a6a6'}} rows="6"></textarea>
                 </div>
                 <button className="btn btn-primary mx-1" onClick={upClick}>Convert to uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={loClick}>Convert to lowercase</button>
                 <button className="btn btn-primary mx-1" onClick={() => {navigator.clipboard.writeText(text)}}>Copy Text</button>
             </div>
-            <div className="container my-4">
+            <div className="container my-4" style= {{color:props.mode==='light'?'black':'white'}}>
                 <h4>Your text Summary</h4>
                 <p>You have {text.length === 0 ? 0 : text.split(" ").length} words and {text.length} characters.</p>
                 <p>{0.008 * parseFloat(text.length === 0 ? 0 : text.split(" ").length)} Minutes to read.</p>
+                <h4>Preview</h4>
+                <p>{text.length===0?'Enter some text in box above':text}</p>
             </div>
         </>
     )
