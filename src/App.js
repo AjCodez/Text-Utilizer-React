8961409b2e805b2e45f6d43ef9ba647b820f1d77
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-19 17:34:40 +0530

    Added alerts

--- src/App.js
+++ src/App.js
@@ -2,35 +2,53 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
+import Alert from './components/Alert';
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message,type) => {
+    setAlert({
+      msg:message,
+      type:type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1000);
+  }
   const toggleModeDark = () => {
       setMode('dark');
       document.body.style.backgroundColor = '#5d5d5d';
+      showAlert("Dark theme has been enabled", "success");
     }
   const toggleModeLight = () => {
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      showAlert("Light theme has been enabled", "success");
     }
   const toggleModeGreen = () => {
       setMode('success');
       document.body.style.backgroundColor = 'rgb(66 183 66)';
+      showAlert("Green theme has been enabled", "success");
     }
   const toggleModeRed = () => {
       setMode('danger');
       document.body.style.backgroundColor = 'rgb(215 96 96)';
+      showAlert("Red theme has been enabled", "success");
     }
   const toggleModeYellow = () => {
       setMode('warning');
       document.body.style.backgroundColor = '#cda734';
+      showAlert("Yellow theme has been enabled", "success");
     }
 
   return (
     <>
       <Navbar title="Hello Aj" about="About Aj" mode={mode} toggleModeDark={toggleModeDark} toggleModeLight={toggleModeLight} toggleModeGreen={toggleModeGreen} toggleModeYellow={toggleModeYellow} toggleModeRed={toggleModeRed}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm mode={mode} />
+        <TextForm mode={mode} showAlert={showAlert}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,10 +5,16 @@ export default function TextForm(props) {
     const upClick = () => {
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Uppercase","success");
     }
     const loClick = () => {
         let newText = text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lowercase","success");
+    }
+    const copyClick = () => {
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to clipboard","success");
     }
     const onChange = (event) => {
         setText(event.target.value);
@@ -24,24 +30,24 @@ export default function TextForm(props) {
                 </div>
                 <button className={`btn btn-${props.mode} mx-1`} onClick={upClick}>Convert to uppercase</button>
                 <button className={`btn btn-${props.mode} mx-1`} onClick={loClick}>Convert to lowercase</button>
-                <button className={`btn btn-${props.mode} mx-1`} onClick={() => { navigator.clipboard.writeText(text) }}>Copy Text</button>
+                <button className={`btn btn-${props.mode} mx-1`} onClick={copyClick}>Copy Text</button>
             </div>
             <div className="container my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h4>Your text Summary</h4>
-                <p>You have {text.length === 0 ? 0 : text.charAt(text.length-1)!==' '?text.split(" ").length:text.split(" ").length-1} words and {text.length} characters.</p>
+                <p>You have {text.length === 0 ? 0 : text.charAt(text.length - 1) !== ' ' ? text.split(" ").length : text.split(" ").length - 1} words and {text.length} characters.</p>
                 <p>{0.008 * parseFloat(text.length === 0 ? 0 : text.split(" ").length)} Minutes to read.</p>
                 <p>
                     <button className="btn btn-info" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
                         View Text
                     </button>
                 </p>
-                </div>
-                <div>
-                    <div className="collapse" id="collapseWidthExample">
-                        <div className="card card-body">
-                        {text.length===0?'Enter some text in box above':text}
-                        </div>
+            </div>
+            <div>
+                <div className="collapse" id="collapseWidthExample">
+                    <div className="card card-body">
+                        {text.length === 0 ? 'Enter some text in box above' : text}
                     </div>
+                </div>
             </div>
         </>
     )
