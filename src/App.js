50d183fc258523148de7cb039fd45d664cff85ad
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2022-03-19 14:42:53 +0530

    formatted

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import React, { useState } from 'react'
 function App() {
   const [mode, setMode] = useState('light');
   const toggleMode = () => {
-      if(mode==='light'){
-        setMode('dark');
-        document.body.style.backgroundColor= 'rgb(38 38 70)';
-      }
-      else{
-        setMode('light');
-        document.body.style.backgroundColor= 'white';
-      }
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'rgb(38 38 70)';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
   }
   return (
     <>
-      <Navbar title="Hello Aj" about="About Aj" mode={mode} toggleMode={toggleMode}/>
+      <Navbar title="Hello Aj" about="About Aj" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
-      <TextForm mode={mode}/> 
+        <TextForm mode={mode} />
       </div>
     </>
   );
